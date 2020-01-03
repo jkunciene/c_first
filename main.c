df7f4118d14b219c5b170a4228c580d909a1cb2b
j.kunciene@gmail.com <48964713+jkunciene@users.noreply.github.com>
2020-01-03 18:33:41 +0200

    c math functions

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 
 int main()
 {
-    printf("My %s number is %d\n", "favorite", 9);
+    printf("My %s number is %d\n", "favorite", 9); //%d decimal
+    printf("%f\n", 5.0 + 4.5); //iskart atieka matematinius veiksmus
+    printf("%f\n", pow(2, 3)); //2 pakelia treciuoju
+    printf("%f\n", sqrt(36));//istraukia sakni
+    printf("%f\n", ceil(36.356));//suapvalina i didesniji sveika skaiciu
+    printf("%f\n", floor(36.356));//suapvalina i mazesni sveikaji skaiciu
+
+    /*mano komentaras
+    variable */
 
     int age = 38;
     char name[] = "Jolita";
